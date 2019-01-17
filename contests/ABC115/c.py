824c8946a0041158189bb68a144a1a2d883cf396
kouma1990 <kouma1990@gmail.com>
2019-01-17 14:35:36 +0900

    update: ABC115 C [TLE -> AC]

--- contests/ABC115/c.py
+++ contests/ABC115/c.py
@@ -2,11 +2,12 @@ import itertools
 
 N, K = (int(i) for i in input().split())  
 heights = [int(input()) for i in range(N)] 
-heights_lists = list(itertools.combinations(heights, K))
 
+heights.sort()
 min_diff = max(heights) - min(heights) 
-for heights_one in heights_lists:
-    if min_diff > (max(heights_one) - min(heights_one) ):
-        min_diff = (max(heights_one) - min(heights_one) )
+for i in range(N-K+1):
+    diff = heights[i+K-1] - heights[i]
+    if diff < min_diff:
+        min_diff = diff
 
 print(min_diff)
