3d52ca703dc6f2ae3af78ff2c6b75e9507a3d500
kouma1990 <kouma1990@gmail.com>
2019-01-23 18:22:36 +0900

    modify: ABC113 C   (AC)

--- contests/ABC113/c.py
+++ contests/ABC113/c.py
@@ -2,16 +2,15 @@ import sys
 input = sys.stdin.readline
  
 N, M = (int(i) for i in input().split())
-input_list = [[int(i) for i in input().split()] for i in range(M)] 
- 
+py_list = [[int(i) for i in input().split()] + [0] for i in range(M)] 
 m_list = [[] for j in range(N+1)]
  
-for inputs in input_list:
-    m_list[inputs[0]].append(inputs[1])
+sorted_py_list = sorted(py_list, key=lambda x:x[1])
+
+city_list = {}
+for py in sorted_py_list:
+    city_list[py[0]] = city_list[py[0]]+1 if py[0] in city_list else 1
+    py[2] = city_list[py[0]]
  
-for arr in m_list:
-    arr.sort()
- 
-for inputs in input_list:
-    num = m_list[inputs[0]].index(inputs[1]) + 1
-    print(str(inputs[0]).zfill(6) + str(num).zfill(6))
+for py in py_list:
+    print(str(py[0]).zfill(6) + str(py[2]).zfill(6))
