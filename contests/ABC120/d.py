74b91cc17bd37cee6c1e350c814650ec2f36725d
kouma1990 <kouma1990@gmail.com>
2019-03-06 11:13:45 +0900

    update: union findの高速化

--- contests/ABC120/d.py
+++ contests/ABC120/d.py
@@ -2,11 +2,16 @@
 class UnionFind():
     def __init__(self, N):
         self.par = [i for i in range(N)]
+        self.size = [1] * N
         self.N = N
 
     # xのrootを返す
     def root(self, x):
-        return x if self.par[x] == x else self.root(self.par[x])
+        if self.par[x] == x:
+            return x
+        else: 
+            self.par[x] = self.root(self.par[x])
+            return self.root(self.par[x])
 
     # x, yが同じrootの場合true
     def same(self, x, y):
@@ -18,20 +23,12 @@ class UnionFind():
     def unite(self, x, y):
         rx = self.root(x)
         ry = self.root(y)
-        if(rx != ry):
-            for i in range(self.N):
-                if self.par[i] == rx:
-                    self.par[i] = ry
-
-def output(uf):
-    count = 0
-    for i in range(uf.N):
-        sub_par = uf.par[i+1:]
-        count += len(sub_par) - sub_par.count(uf.par[i])
-
-    return count
-
-
+        if self.size[rx] < self.size[ry]:
+            self.par[rx] = ry
+            self.size[ry] += self.size[rx]
+        else:
+            self.par[ry] = rx
+            self.size[rx] += self.size[ry]
 
 n, m = (int(i) for i in input().split())
 input_arr = [[int(i) for i in input().split()] for i in range(m)] 
@@ -40,14 +37,16 @@ uf = UnionFind(n)
 #input_arr = [[1,2],[3,4],[1,3],[2,3],[1,4]]
 
 #print(output(uf))
-result = [output(uf)]
+result = [int(n*(n-1)/2)]
 for i in input_arr[::-1]:
     a = i[0]-1
     b = i[1]-1
-    uf.unite(a,b)
-    result.append(output(uf))
-    #print(output(uf))
-    #print("{},{}:{}:{}".format(a, b, uf.par, output(uf) ))
+    if not uf.same(a,b):
+        x = uf.size[uf.root(a)] * uf.size[uf.root(b)]
+        uf.unite(a,b)
+        result.append(int(result[-1]-x))
+    else:
+        result.append(result[-1])
 
 for i in result[::-1][1:]:
     print(i)
--- example/union_find.py
+++ example/union_find.py
@@ -2,11 +2,16 @@
 class UnionFind():
     def __init__(self, N):
         self.par = [i for i in range(N)]
+        self.size = [1] * N
         self.N = N
 
     # xのrootを返す
     def root(self, x):
-        return x if self.par[x] == x else self.root(self.par[x])
+        if self.par[x] == x:
+            return x
+        else: 
+            self.par[x] = self.root(self.par[x])
+            return self.root(self.par[x])
 
     # x, yが同じrootの場合true
     def same(self, x, y):
@@ -18,19 +23,14 @@ class UnionFind():
     def unite(self, x, y):
         rx = self.root(x)
         ry = self.root(y)
-        if(rx != ry):
-            for i in range(self.N):
-                if self.par[i] == rx:
-                    self.par[i] = ry
-
+        if self.size[rx] < self.size[ry]:
+            self.par[rx] = ry
+            self.size[ry] += self.size[rx]
+        else:
+            self.par[ry] = rx
+            self.size[rx] += self.size[ry]
+            
 #　以降 ABC120の例 (途中まで)--------------------------------------------
-def output(uf):
-    count = 0
-    for i in range(uf.N):
-        sub_par = uf.par[i+1:]
-        count += len(sub_par) - sub_par.count(uf.par[i])
-
-    return count
 
 
 if __name__ == '__main__':
@@ -40,17 +40,19 @@ if __name__ == '__main__':
     input_arr = [[1,2],[3,4],[1,3],[2,3],[1,4]]
 
 
-    #print(output(uf))
-    result = [output(uf)]
+    result = [int(uf.N*(uf.N-1)/2)]
     for i in input_arr[::-1]:
         a = i[0]-1
         b = i[1]-1
-        uf.unite(a,b)
-        result.append(output(uf))
-        #print(output(uf))
-        #print("{},{}:{}:{}".format(a, b, uf.par, output(uf) ))
+        if not uf.same(a,b):
+            x = uf.size[uf.root(a)] * uf.size[uf.root(b)]
+            uf.unite(a,b)
+            result.append(int(result[-1]-x))
+        else:
+            result.append(int(result[-1]))
+        print("{},{}:{}:{}".format(a, b, uf.par, result[-1] ))
+        
 
     for i in result[::-1][1:]:
         print(i)
-
-    #print(uf.par)
+        
