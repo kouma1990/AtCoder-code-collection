e495c238f8e4e233627f564a7bbf1e8cc32a8c2b
kouma1990 <kouma1990@gmail.com>
2019-03-08 18:22:52 +0900

    ABC117 d WA

---
+++ contests/ABC117/d.py
@@ -0,0 +1,37 @@
+
+n = 3
+k = 7
+a = [1, 6, 3]
+
+n = 4
+k = 9
+a = [7,4,0,3]
+
+n = 1
+k = 0
+a = [1000000000000]
+k_bins = len(bin(k))-2
+count_0 = [0]*k_bins
+count_1 = [0]*k_bins
+for a_ in a:
+    a_bin = bin(a_)
+    a_bins = len(a_bin) - 2
+    for i in range(k_bins):
+        if (i+1 > a_bins) or (a_bin[-i-1] == '0'):
+            count_0[i] += 1
+        else:
+            count_1[i] += 1
+
+x_bin = ""
+for i in range(k_bins):
+    if count_0[i] > count_1[i]:
+        x_bin = "1" + x_bin
+    else:
+        x_bin = "0" + x_bin
+
+x = int("0b"+x_bin, 0)
+result = 0
+for a_ in a:
+    result += a_ ^ x
+print(result)
+
