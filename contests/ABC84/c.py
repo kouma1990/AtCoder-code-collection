03706a6c5c4681932317329f4226763e5918c400
kouma1990 <kouma1990@gmail.com>
2019-03-15 13:51:15 +0900

    delete: 不要なコード

--- contests/ABC84/c.py
+++ contests/ABC84/c.py
@@ -15,28 +15,3 @@ for i in range(n-1):
 result.append(0)
 for x in result:
     print(x)
-
-"""
-if n==1:
-    print(0)
-    exit()
-
-result = [0, e[-1][0]+e[-1][1]]
-if n==2:
-    for x in result[::-1]:
-        print(x)
-    exit()
-
-if n > 2:
-    for i in range(1,n-1):
-        a = e[-i-1][0] + e[-i-1][1]
-        if a <= e[-i][1]:
-            result.append(result[-1]) 
-            continue
-        b = math.ceil((a - e[-i][1]) / e[-i][2])
-        result.append(result[-1]+b* e[-i][2])
-        #print(e[-i])
-
-for x in result[::-1]:
-    print(x)
-"""
