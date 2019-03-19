4de68946fc797d95134148c3537367eeddbc1ce7
kouma1990 <kouma1990@gmail.com>
2019-03-19 18:15:01 +0900

    delete: 不要な処理 ABC66 c

--- contests/ABC66/c.py
+++ contests/ABC66/c.py
@@ -11,22 +11,3 @@ else:
 
 res = list(map(str, res))
 print(' '.join(res))
-#print(x)
-#print(y)
-
-exit()
-flg = True
-if n % 2 == 0:
-    flg = False
-
-b = []
-
-for x in a:
-    if flg:
-        b.insert(0, str(x))
-    else:
-        b.append(str(x))
-
-    flg = not flg
-
-print(' '.join(b))
