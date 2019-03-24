65db009271d93dd0c9ad9aa93c357a1c180c5580
kouma1990 <kouma1990@gmail.com>
2019-03-24 20:27:53 +0900

    add: ABC59 a,b,c (c途中)

---
+++ contests/ABC59/a.py
@@ -0,0 +1,3 @@
+a, b, c = (i for i in input().split())
+
+print(a.upper()[0]+b.upper()[0]+c.upper()[0])
---
+++ contests/ABC59/c.py
@@ -0,0 +1,12 @@
+n = int(input())
+a = [int(i) for i in input().split()]
+
+current = a[0]
+res = 0
+"""
+for i in range(1,n):
+    flg = 
+
+
+print(res)
+"""
