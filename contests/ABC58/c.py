f5d69517e9d088d31ed8382c7427996c1731e40f
kouma1990 <kouma1990@gmail.com>
2019-03-25 11:34:18 +0900

    add: ABC58 c

--- contests/ABC58/c.py
+++ contests/ABC58/c.py
@@ -1,28 +1,18 @@
 n = int(input())
 s = [input() for i in range(n)]
 
+m = "abcdefghijklmnopqrstuvwxyz"
+count = []
+
+for x in m:
+    cnt = 100
+    for si in s:
+        cnt = min(cnt,si.count(x))
+
+    count.append(cnt)
+
 res = ""
-for i in range(len(s[0])):
-    l = len(s[0]) - i
-    for j in range(len(s[0])):
-        if j+l > len(s[0]):
-            break
-
-        cs = s[0][j:j+l]
-        print(cs)
-        flg = True
-        for x in s[1:]:
-            if x.count(cs) == 0:
-                flg = False
-                break
-
-        if flg:
-            if res == "":
-                res = cs
-            else:
-                res = min(res,cs)
-        
-    if res != "":
-        break
+for i in range(26):
+    res += m[i]*count[i]
 
 print(res)
